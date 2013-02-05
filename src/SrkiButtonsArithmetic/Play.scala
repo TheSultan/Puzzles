6a0dbe1492f7e248d6586d62e6643fdc7e7a4977
BW L33t <bwhackathon@gmail.com>
2013-02-04 20:27:11 -0500

    Fixed version. Refactored to modularize.

--- src/SrkiButtonsArithmetic/Play.scala
+++ src/SrkiButtonsArithmetic/Play.scala
@@ -1,8 +1,8 @@
 import scala.collection._
 
 /*
-   0 1
-   2 3
+  Puzzle courtesy of Srdjan
+  Original source (spoiler / solution alert): http://arcsecond.wordpress.com/2013/01/21/spinning-room-puzzle/
  */
 class Board(buttons: mutable.Buffer[Boolean]) {
 
@@ -14,77 +14,54 @@ class Board(buttons: mutable.Buffer[Boolean]) {
 
   private def s(i: Int) = if (buttons(i)) "X" else "0"
 
-
+  /*
+   0 1
+   2 3
+   */
   override def toString = "\t" + s(0) + s(1) + "\n\t" + s(2) + s(3) + "\n"
 }
 
 
-for (i <- 0 to 15
-  //filter(_ == 3)
-) {
+for (i <- 0 to 15) {
   val buttons = for (j <- 0 to 3) yield if ((i & (math.pow(2, j).toInt)) != 0) true else false
   val board = new Board(buttons.toBuffer)
+  var moves = 0
+  var won = false
 
   def printBoard(force: Boolean = false) = {
     if (force) println(board.toString)
   }
 
-  println("Configuration " + i)
-  printBoard(true)
-
-  def solveEvens(iteration: Int = 1) = {
-    def makeMoveUnlessWon(move: () => Unit) = {
-      if (board.hasWon())
-        println("Won in " + iteration)
-      else {
-        move()
-        printBoard()
-      }
-    }
-
-    makeMoveUnlessWon(board.flipDiagonal _)
-
+  def hasWon() : Boolean = {
     if (board.hasWon()) {
-      println("Won in " + iteration)
-    } else {
-      board.flipDiagonal()
+      if (!won) {
+        println("Won in " + moves + " moves")
+        won = true
+      }
+      return true
+    }
+    return false
+  }
+
+  def makeMoveUnlessWon(move: () => Unit) = {
+    if (!hasWon()) {
+      move()
+      moves += 1
       printBoard()
-      if (board.hasWon()) {
-        println("Won in " + (iteration + 1) )
-      } else {
-        board.flipVertical()
-        printBoard()
-        if (board.hasWon()) {
-          println("Won in " (iteration + 2))
-        } else {
-          board.flipDiagonal()
-          printBoard()
-          if (board.hasWon()) {
-            println("Won in " + (iteration + 3))
-          }
-        }
-      }
     }
   }
 
-  if (board.hasWon()) {
-    println("Won in 0")
-  } else {
-    solveEvens()
-    if (board.hasWon()) {
-      println("Won in 3")
-    } else {
-      board.flipOne()
-      if (board.hasWon()) {
-        println("Won in 4")
-      } else {
-        printBoard()
-        solveEvens(4)
-        if (!board.hasWon()) {
-          println("FAIL!!!")
-          printBoard()
-        }
-      }
-    }
+  println("Configuration " + i)
+  //printBoard(true)
+
+  def solveEvens() = {
+    makeMoveUnlessWon(board.flipDiagonal _)
+    makeMoveUnlessWon(board.flipVertical _)
+    makeMoveUnlessWon(board.flipDiagonal _)
   }
+
+  solveEvens()
+  makeMoveUnlessWon(board.flipOne _)
+  solveEvens()
+  if (!hasWon()) println("FAIL!!!")
 }
